ea2319e1efdf9150092aabf324527147b4d7de30
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-04 18:29:09 +0100

    when user clicks main container cart is closed

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,12 @@
 const cartIcon = document.getElementById('nav-cart-toggler');
 const cartContainer = document.getElementById('nav-cart-container');
 
+const main = document.querySelector("#main");
+
 cartIcon.addEventListener('click', () => {
     cartContainer.classList.toggle('active-cart')
+})
+
+main.addEventListener('click', () => {
+    cartContainer.classList.remove('active-cart')
 })
