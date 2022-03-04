b2a2817a43c206fc28a1c51a594d231384e8dbca
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-04 17:31:12 +0100

    closing menu on nav-close-menu click

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,17 @@
 const menuIcon = document.getElementById("nav-menu-toggler");
 const pageLinks = document.getElementById("nav-page-links");
+const closeIcon = document.getElementById("nav-close-menu");
 
+// OPEN MENU
 menuIcon.addEventListener('click', openMenu);
 
-function openMenu(e) {
+function openMenu() {
     pageLinks.classList.add("active-mobile");
+}
+
+// CLOSE MENU
+closeIcon.addEventListener('click', closeMenu);
+
+function closeMenu() {
+    pageLinks.classList.remove("active-mobile");
 }
