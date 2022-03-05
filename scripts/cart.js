d28f0559cfd35efa4981aea13055b339cf54ac6b
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-05 11:05:30 +0100

    added content for filled cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,7 +1,14 @@
+// CART CONTAINER
 const cartIcon = document.getElementById('nav-cart-toggler');
 const cartContainer = document.getElementById('nav-cart-container');
 
-const main = document.querySelector("#main");
+// ADD TO CART CONTAINER 
+const decreaseQnt = document.getElementById("quantity__decrease");
+const increaseQnt = document.getElementById("quantity__increase");
+const quantity = document.getElementById("current-quantity");
+const addToCartBtn = document.getElementById('add-to-cart');
+
+// CART CONTAINER
 
 cartIcon.addEventListener('click', () => {
     cartContainer.classList.toggle('active-cart')
@@ -9,3 +16,29 @@ cartIcon.addEventListener('click', () => {
 
 // close cart on click outside of cart
 // window.addEventListener('click', e => e.target === cartContainer ? cartContainer.classList.remove('active-cart') : false)
+
+// ADD TO CART
+let currentQnt = 0;
+
+decreaseQnt.addEventListener("click", () => {
+    currentQnt--;
+    if (currentQnt < 0) {
+        currentQnt = 0;
+    }
+    quantity.textContent = currentQnt;
+});
+
+increaseQnt.addEventListener("click", () => {
+    currentQnt++;
+    quantity.textContent = currentQnt;
+});
+
+
+
+addToCartBtn.addEventListener("click", () => {
+    if (currentQnt > 0) {
+        console.log(currentQnt);
+    } else {
+        alert("Quantity cannot be 0.")
+    }
+});
